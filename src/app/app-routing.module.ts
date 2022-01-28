e38710a5bb390f1c9a31c215deb3df5962668b11
Sergey <gitkhartmann.com>
2022-01-28 17:37:56 +0300

    изучил модули

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { HomePageComponent } from './home-page/home-page.component'
+
+
+@NgModule({
+	imports: [RouterModule.forRoot([
+		{ path: '', component: HomePageComponent, pathMatch: 'full' },
+		{ path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule) }
+	])],
+	exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { FormsModule } from '@angular/forms'
+import { HomePageComponent } from './home-page/home-page.component'
+import { AppRoutingModule } from './app-routing.module'
+import { SharedModule } from './shared/shared.module'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomePageComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AppRoutingModule,
+		SharedModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
